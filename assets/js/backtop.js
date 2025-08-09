f3986185a1b6de691fea5ddcb155afa117978b39
it27427 <istiak.tushar.qtec@gmail.com>
2025-08-09 11:34:09 +0600

    backtotop-done

--- assets/js/backtop.js
+++ assets/js/backtop.js
@@ -1,9 +1,9 @@
 up({
-  bottom: "2rem",
+  bottom: "2.25rem",
   right: "1.25rem",
   width: "2.5rem",
   height: "2.5rem",
-  bg: "#a100ff",
+  bg: "#2d4d3a",
   src: "../assets/cursor/chevron-up-solid.svg",
   whenShow: 400,
   circle: true,
