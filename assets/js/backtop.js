bf34eee761f265c31f9535275d340101785dd876
it27427 <istiak.tushar.qtec@gmail.com>
2025-08-10 18:43:13 +0600

    back-to-top updated

--- assets/js/backtop.js
+++ assets/js/backtop.js
@@ -4,7 +4,7 @@ up({
   width: "2.5rem",
   height: "2.5rem",
   bg: "#2d4d3a",
-  src: "../assets/cursor/chevron-up-solid.svg",
+  src: "../cursor/chevron-up-solid.svg",
   whenShow: 400,
   circle: true,
 });
