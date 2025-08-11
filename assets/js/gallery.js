f4875851da1c0cc6ee854d8883b1916e6b0b724c
it27427 <istiak.tushar.qtec@gmail.com>
2025-08-12 05:09:06 +0600

    logo and headerupdaed

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,13 +1,27 @@
-baguetteBox.run(".gallery", {
+baguetteBox.run(".gallery-1", {
   captions: true,
   buttons: true,
   fullScreen: false,
   noScrollbars: false,
   titleTag: true,
-  // bodyClass: 'baguetteBox-open',
+  bodyClass: "baguetteBox-open",
   titleTag: false,
   async: false,
   preload: 2,
   animation: "slideIn",
-  overlayBackgroundColor: "rgba(0,0,0,.75)",
+  overlayBackgroundColor: "rgba(18, 17, 16, 0.5)",
+});
+
+baguetteBox.run(".gallery-2", {
+  captions: true,
+  buttons: true,
+  fullScreen: false,
+  noScrollbars: false,
+  titleTag: true,
+  bodyClass: "baguetteBox-open",
+  titleTag: false,
+  async: false,
+  preload: 2,
+  animation: "slideIn",
+  overlayBackgroundColor: "rgba(18, 17, 16, 0.5)",
 });
