758ded2ce6e8563cd45863b930032b78c79640f9
it27427 <istiak.tushar.qtec@gmail.com>
2025-08-12 02:22:28 +0600

    section-text updated

---
+++ assets/js/gallery.js
@@ -0,0 +1,13 @@
+baguetteBox.run(".gallery", {
+  captions: true,
+  buttons: true,
+  fullScreen: false,
+  noScrollbars: false,
+  titleTag: true,
+  // bodyClass: 'baguetteBox-open',
+  titleTag: false,
+  async: false,
+  preload: 2,
+  animation: "slideIn",
+  overlayBackgroundColor: "rgba(0,0,0,.75)",
+});
